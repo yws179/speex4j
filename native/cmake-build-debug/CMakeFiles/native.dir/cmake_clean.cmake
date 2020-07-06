476f15d183653ffa4579074f22fc1255702abca4
yws <yws179@gmail.com>
2020-07-06 23:25:52 +0800

    inital

---
+++ native/cmake-build-debug/CMakeFiles/native.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/native.dir/main.cpp.o"
+  "native"
+  "native.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/native.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ speex4j/src/com/github/yws179/Test.java
@@ -0,0 +1,11 @@
+package com.github.yws179;
+
+/**
+ * @author yws
+ * @date 2020/07/06
+ */
+public class Test {
+
+    public native String getMsg();
+
+}
